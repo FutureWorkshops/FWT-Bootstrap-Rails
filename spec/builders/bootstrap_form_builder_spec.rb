d56b94ae077b42b1d49ec134d4c3e9873f7b1d8f
Matt Brooke-Smith <matt@futureworkshops.com>
2014-08-03 18:04:40 +0100

    Spec

--- spec/builders/bootstrap_form_builder_spec.rb
+++ spec/builders/bootstrap_form_builder_spec.rb
@@ -5,25 +5,37 @@ class TestHelper < ActionView::Base; end
 
 describe BootstrapFormBuilder do 
   
+  let(:helper)   { TestHelper.new }
+  let(:resource) { User.new }
+  let(:builder)  { BootstrapFormBuilder.new :user, resource, helper, {} }
+  
   describe '#checkbox' do
+    let(:output) { builder.check_box :administrator }
 
-    let(:helper) { TestHelper.new }
-    let(:resource)  { User.new }
-    let(:builder) { BootstrapFormBuilder.new :user, resource, helper, {} }
-    let(:output)    {
-     builder.check_box(:administrator)
-    }
-
-    it 'wraps input and label' do
-     expect(output).to have_tag('input', :with => { :id => :user_administrator })
+    it 'creates a checkbox input' do
+     expect(output).to have_tag('input', :with => { :id => :user_administrator, :type => :checkbox })
     end
 
     it 'creates a label' do
      expect(output).to include '<label'
     end
    end
+   
+   describe '#hidden' do
+
+     let(:output) { builder.hidden_field :administrator }
+
+     it 'creates a hidden input' do
+      expect(output).to have_tag('input', :with => {:type => :hidden, :id => :user_administrator, :value => true})
+     end
+   end
+    
 end
 
 class User
   attr_accessor :administrator
+  
+  def administrator
+    true
+  end
 end
